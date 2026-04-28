22533b1c3f302a62c485ab2c5d4faf295a4e31e5
skyluw <clcm2512@gmail.com>
2026-04-28 02:41:00 -0500

    og image y metadata actualizada

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,31 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Fiera Lab",
-  description: "Fiera Lab crea experiencias, activaciones y producciones BTL con impacto real.",
+  description: "Fiera Lab es el laboratorio donde la estrategia se vuelve brutal y la creatividad incomoda al mercado. Agencia BTL para marcas que quieren crecer en serio. Próximamente..",
   icons: {
     icon: '/favicon.svg',
+  },
+  openGraph: {
+    title: "Fiera Lab",
+    description: "Fiera Lab es el laboratorio donde la estrategia se vuelve brutal y la creatividad incomoda al mercado. Agencia BTL para marcas que quieren crecer en serio. Próximamente.",
+    url: "https://www.fieralab.pe",
+    siteName: "Fiera Lab",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Fiera Lab — Agencia BTL",
+      }
+    ],
+    locale: "es_PE",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fiera Lab",
+    description: "Fiera Lab es el laboratorio donde la estrategia se vuelve brutal y la creatividad incomoda al mercado. Agencia BTL para marcas que quieren crecer en serio. Próximamente.",
+    images: ["/og-image.png"],
   },
 };
 
